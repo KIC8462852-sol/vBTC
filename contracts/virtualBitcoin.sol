120388257f565c11edf27db3b960fd94b03e762c
KIC8462852-sol <tabithastar@protonmail.com>
2020-03-16 17:29:46 +0930

    SafeMath: Mulitplication and Division cannot overflow

--- contracts/virtualBitcoin.sol
+++ contracts/virtualBitcoin.sol
@@ -1,20 +1,43 @@
 pragma solidity >=0.5.0;
 
+// ERC-20 Interface
+// interface ERC20 {
+//     function totalSupply() external view returns (uint256);
+//     function balanceOf()
+// }
 
 //SafeMath library
 library SafeMath {
 
-        // SafeMath: Addition overflow
+        // SafeMath: Addition cannot overflow
         function add(uint256 a, uint256 b) internal pure returns (uint256) {
         uint256 c = a + b;
         require(c >= a, "SafeMath: addition overflow");
 
         return c;
     }
-        // SafeMath: subtraction overflow
+        // SafeMath: Subtraction cannot overflow
         function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
         require(b <= a, errorMessage);
         uint256 c = a - b;
+
+        return c;
+    }
+        // SafeMath: Multiplication cannot overflow
+        function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+        // SafeMath: Division cannot overflow
+        function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b > 0, errorMessage);
+        uint256 c = a / b;
 
         return c;
     }
@@ -25,43 +48,31 @@ library SafeMath {
 contract virtualBitcoin {
 
     using SafeMath for uint256;
+
+    // Variables
     string public name = "VirtualBitcoin";
     string public symbol = "vBTC";
     string public standard = "vBTC Token v1.0";
     uint256 public totalSupply;
 
-    // Transfer event defined
-    event Transfer(
-        address indexed _from,
-        address indexed _to,
-        uint256 _value
-    );
-
-    // Approval event defined
-    event Approval(
-        address indexed _owner,
-        address indexed _spender,
-        uint256 _value
-    );
-
     // Mappings
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping (address => uint256 )) public allowance;
 
-    //Set initial token supply
+    // Transfer event defined
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    // Approval event defined
+    event Approval(address indexed _owner, address indexed _spender,uint256 _value);
+
+    // Set initial token supply
     constructor(uint256 _initialSupply) public {
-        balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
+        balanceOf[msg.sender] = _initialSupply;
+        emit Transfer(address(0), msg.sender, _initialSupply);
     }
     //Transfer function
     // function transfer(address _to, uint256 _value) public returns (bool success) {
-    //     require(balanceOf[msg.sender] >= _value);
-
-    //     balanceOf[msg.sender] -= _value;
-    //     balanceOf[_to] += _value;
-
-    //     emit Transfer(msg.sender, _to, _value);
-
     //     return true;
     // }
 
