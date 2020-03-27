dab82483cba6f3e718d9238dd8863da5e69b1949
strictly-scarce <dev@ssdev.local>
2020-03-27 16:15:25 +1100

    Resolve Issue11

--- src/App.js
+++ src/App.js
@@ -4,24 +4,41 @@ import './App.css';
 import { VBTC_ADDR, VBTC_ABI } from './contract-abi'
 import Web3 from 'web3'
 
+import { Button } from 'antd'
+
 const App = (props) => {
 
 const [contract, setContract] = useState(null)
-const [account, setAccount] = useState(null)
+
+const [account0, setAccount0] = useState(
+  {address:'', tokenBalance:'', ethBalance:''})
+
+const [account1, setAccount1] = useState(
+    {address:'', tokenBalance:'', ethBalance:''})
+
 const [tokenData, setTokenData] = useState(
     {name:'', symbol:'', totalSupply:'', decimals:''})
 
 useEffect(() => {
 
   const loadBlockchainData = async () => {
-    const web3 = new Web3(
-      new Web3.providers.HttpProvider('http://127.0.0.1:8545')
-    );
+    const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'));
     const accounts = await web3.eth.getAccounts()
-    setAccount(accounts[0])
+    var balance_ = await web3.eth.getBalance(accounts[0])
+    var bal_ = convertToWei(balance_)
 
     const contract_ = new web3.eth.Contract(VBTC_ABI, VBTC_ADDR)
     setContract(contract_)
+
+    const tokenBalance_ = await contract_.methods.balanceOf(accounts[0]).call()
+    
+    setAccount0({
+      address: accounts[0],
+      tokenBalance: tokenBalance_,
+      ethBalance: bal_
+    })
+
+  
 
     const name_ = await contract_.methods.name().call()
     const symbol_ = await contract_.methods.symbol().call()
@@ -38,35 +55,96 @@ useEffect(() => {
   loadBlockchainData()
 }, [])
 
-function convertToNumber(number){
-    var num = number / 1
-    // num = num.toFixed(2)
-    return num
+  const transferFunc = async () => {
+    console.log("we clicked")
+    const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'));
+    const accounts = await web3.eth.getAccounts()
+    const contract_ = new web3.eth.Contract(VBTC_ABI, VBTC_ADDR)
+
+    const to_= accounts[1]
+    const val_ = '1000000'
+    const resp = await contract_.methods.transfer(to_, val_).send({from: accounts[0]})
+    updateAccount1()
   }
 
+const updateAccount1 = async () => {
 
-   const setData = async e => {
-    e.preventDefault();
-    const gas = await contract.methods.transfer(0x41050679dDC39E12eE7A494df554C154D49Ced74, amount).estimateGas();
-    const tx = await contract.methods
-      .transfer(0x41050679dDC39E12eE7A494df554C154D49Ced74, amount)
-      .send({ from: account, gas });
-    console.log(tx);
-  };
+  const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'));
+  const accounts = await web3.eth.getAccounts()
+  var balance_ = await web3.eth.getBalance(accounts[1])
+  var bal_ = convertToWei(balance_)
+  const contract_ = new web3.eth.Contract(VBTC_ABI, VBTC_ADDR)
+  const tokenBalance_ = await contract_.methods.balanceOf(accounts[1]).call()
+  console.log(tokenBalance_)
+    
+    setAccount1({
+      address: accounts[1],
+      tokenBalance: tokenBalance_,
+      ethBalance: bal_
+    })
+}
 
+const sendEth = async () => {
+  const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'));
+  const accounts = await web3.eth.getAccounts()
+
+  const fromAcc = accounts[0]
+  const toAcc = accounts[1]
+  const amount = 100000000000000000
+
+  const tx = await web3.eth.sendTransaction({ from: fromAcc, to: toAcc, value: amount })
+  console.log(tx)
+
+  var balance_ = await web3.eth.getBalance(accounts[1])
+  var bal_ = convertToWei(balance_)
+  const contract_ = new web3.eth.Contract(VBTC_ABI, VBTC_ADDR)
+  const tokenBalance_ = await contract_.methods.balanceOf(accounts[1]).call()
+
+  setAccount1({
+    address: accounts[1],
+    tokenBalance: tokenBalance_,
+    ethBalance: bal_
+  })
+}
+
+function convertToNumber(number){
+  var num = number / 1
+  // num = num.toFixed(2)
+  return num
+}
+
+function convertToWei(number){
+  var num = number / 1000000000000000000
+  return num.toFixed(2)
+}
 
   return (
     <div className="App">
       <header className="App-header">
 
-        <p>Your account: {account}</p>
+        <p>Your account: {account0.address}</p>
+        <p>Your Token balance: {account0.tokenBalance.toLocaleString()}</p>
+        <p>Your eth balance: {account0.ethBalance}</p>
+
         <p>Token Name: {tokenData.name}</p>
         <p>Token Symbol: {tokenData.symbol}</p>
         <p>Token Supply: {tokenData.totalSupply.toLocaleString()}</p>
         <p>Token Decimals: {tokenData.decimals}</p>
 
+        <Button type="primary" loading={false} onClick={transferFunc}>
+          Transfer VBTC to Acc1
+        </Button>
 
-        <form onSubmit={setData}>
+        <Button type="primary" loading={false} onClick={sendEth}>
+          Transfer 1 Eth
+        </Button>
+
+        <p>Their account: {account1.address}</p>
+        <p>Their Token balance: {account1.tokenBalance.toLocaleString()}</p>
+        <p>Their eth balance: {account1.ethBalance}</p>
+
+
+        {/* <form onSubmit={setData}>
           <label>
             Set Data:
             <input
@@ -77,7 +155,7 @@ function convertToNumber(number){
             />
           </label>
           <input type="submit" value="Set Data" />
-        </form>
+        </form> */}
 
         
       </header>
