bee936fad9ca0fdde9ba7bafbd943722a3b8ede6
KIC8462852-sol <62094398+KIC8462852-sol@users.noreply.github.com>
2020-04-16 15:22:21 +0930

    Issue17 (#23)
    * Replaced VETHER with VIRTUAL BITCOIN
    
    * Re arranged era.js, and a changed veth to vbtc in acquire
    
    * Adjustment to the introduction paragraph
    
    * more additions
    
    * wip
    
    * initial work
    
    * add contract-abi back
    
    * added Era-web3.js and modified Era.js to suite
    
    * added two new web3 files and adjusted files with relationships
    
    * changed era table a bit
    
    Co-authored-by: Strictly Scarce <strictly_scarce1@protonmail.com>

--- src/components/Acquire.js
+++ src/components/Acquire.js
@@ -22,7 +22,7 @@ const Acquire = (props) => {
 		<div>
 			
 			<Gap />
-			<H2>ACQUIRE VETHER</H2><br />
+			<H2>ACQUIRE VIRTUAL BITCOIN</H2><br />
 			<Subtitle>Acquire a share of today’s emission by burning Ether.</Subtitle>
 			<br />
 			<Text>Provably destroy capital to acquire a fair share. Capital is measured in units of Ether after burning.</Text><br />
